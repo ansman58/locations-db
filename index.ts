92d3e44bc2e6cea6b091b055a766516adf2532cb
ansman58 <javanslem@gmail.com>
2024-06-01 02:47:36 +0100

    update

--- index.ts
+++ index.ts
@@ -21,52 +21,57 @@ dotenv.config();
 })();
 
 (async () => {
-  const country = await Country.findOne({ name: "Nigeria" });
-  const countryId = country?._id;
-
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  // Navigate the page to a URL
-  await page.goto(
-    "https://en.wikipedia.org/wiki/Local_government_areas_of_Nigeria"
-  );
-
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
-
-  await page.screenshot({ path: "screenshot.png" });
-
-  const statesAndLGAs = await page.$$eval("h3", (states) =>
-    states.map((state) => {
-      const h3Text = state.textContent?.split("[")?.[0] || "";
-      const nextElement = state.nextElementSibling;
-      const secondNextElement = nextElement?.nextElementSibling;
-
-      // Check if the next elements exist and get their text content
-      const cities =
-        nextElement?.tagName === "LINK" && secondNextElement?.tagName === "DIV"
-          ? secondNextElement.textContent?.split("\n").filter(Boolean)
-          : ["No following div"];
-
-      return { state: h3Text, cities };
-    })
-  );
-
-  for (const state of statesAndLGAs) {
-    console.log(state.state?.split(" ")[0]);
-    const st = await State.create({
-      name: state.state?.split(" ")[0],
-      countryId: countryId,
-    });
-
-    if (!state.cities) return;
-
-    for (const city of state.cities) {
-      await LGA.create({ name: city, stateId: st._id });
+  try {
+    // Retrieve country details
+    const country = await Country.findOne({ name: "Nigeria" });
+    if (!country) {
+      throw new Error("Country not found");
     }
-  }
+    const countryId = country._id;
 
-  await browser.close();
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    await page.goto(
+      "https://en.wikipedia.org/wiki/Local_government_areas_of_Nigeria",
+      {
+        waitUntil: "networkidle2",
+      }
+    );
+
+    // Extract states and LGAs
+    const statesAndLGAs = await page.$$eval("h3", (states) =>
+      states.map((state) => {
+        const h3Text = state.textContent?.split("[")?.[0] || "";
+        const nextElement = state.nextElementSibling;
+        const secondNextElement = nextElement?.nextElementSibling;
+
+        const cities =
+          nextElement?.tagName === "LINK" &&
+          secondNextElement?.tagName === "DIV"
+            ? secondNextElement.textContent?.split("\n").filter(Boolean)
+            : [];
+
+        return { state: h3Text, cities };
+      })
+    );
+
+    for (const data of statesAndLGAs) {
+      const state = await State.create({
+        name: data.state.split(" ")[0],
+        countryId: countryId,
+      });
+
+      if (data.cities) {
+        for (const city of data.cities) {
+          await LGA.create({ name: city, stateId: state._id });
+        }
+      }
+    }
+
+    await browser.close();
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 })();
